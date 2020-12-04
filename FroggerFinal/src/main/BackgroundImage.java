98a971159c7c3ef93e4ac5640d9430cd0f903580
Jack <jackchew98@gmail.com>
2020-12-04 21:14:58 +0800

    Initial commit

---
+++ FroggerFinal/src/main/BackgroundImage.java
@@ -0,0 +1,17 @@
+package main;
+
+import javafx.scene.image.Image;
+
+public class BackgroundImage extends Actor {
+
+	@Override
+	public void act(long now) {
+
+	}
+
+	public BackgroundImage(String imageLink) {
+		setImage(new Image(imageLink, 600, 800, true, true));
+
+	}
+
+}
